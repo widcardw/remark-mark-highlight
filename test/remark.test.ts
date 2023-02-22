6bb4813cfd8fdae82f1c15abd4cedc204565a7bd
widcardw <lui747353367@qq.com>
2023-02-22 17:04:10 +0800

    fix: transform mark highlight to span with classes

--- test/remark.test.ts
+++ test/remark.test.ts
@@ -1,18 +1,32 @@
-import { remark } from 'remark'
+import { unified } from 'unified'
 import { describe, it, expect } from 'vitest'
 import { remarkMark } from '../lib/mdast/index'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import { removePosition } from 'unist-util-remove-position'
+
 
 const md1 = 'a ==b=='
 
 describe('remark', () => {
-  it('should parse markdown', async () => {
-    const proc = remark().use(remarkMark).freeze()
-    const tree = proc.parse(md1)
-    expect(tree).toMatchSnapshot()
-    const vf = await proc.process(md1)
-    expect(String(vf)).toMatchInlineSnapshot(`
-      "a ==b==
-      "
-    `)
+  it('should generate ast', () => {
+    expect(
+      removePosition(
+        unified()
+          .use(remarkParse)
+          .use(remarkMark)
+          .parse(md1),
+        true))
+      .toMatchSnapshot()
+  })
+  it('should parse markdown', () => {
+    const toHtml = unified()
+      .use(remarkParse)
+      .use(remarkMark)
+      .use(remarkRehype)
+      .use(rehypeStringify)
+
+    expect(toHtml.processSync(md1).toString()).toMatchInlineSnapshot('"<p>a <span class=\\"mark\\">b</span></p>"')
   })
 })
